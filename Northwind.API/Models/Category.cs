4474f4b6a96bdec79e17556052f41f1f3f927fb1
Flintstone <beginflints@gmail.com>
2020-06-24 22:42:23 +0700

    Edit Category

--- Northwind.API/Models/Category.cs
+++ Northwind.API/Models/Category.cs
@@ -5,6 +5,11 @@ namespace DatingApp.API.Models
     [Table("Catagories")]
     public class Category
     {
-        public int MyProperty { get; set; }
+        public int CategoryID { get; set; }
+        [Column(TypeName="nvarchar(15)")]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        [Column(TypeName="image")]
+        public byte[] MyProperty { get; set; }
     }
 }
