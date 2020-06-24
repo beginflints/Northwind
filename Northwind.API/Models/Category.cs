c633dc277d41a072993eba8bf502cf8133a3ab6e
Flintstone <beginflints@gmail.com>
2020-06-24 22:37:17 +0700

    Edit name

--- Northwind.API/Models/Category.cs
+++ Northwind.API/Models/Category.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace DatingApp.API.Models
 {
     [Table("Catagories")]
-    public class Catagory
+    public class Category
     {
         public int MyProperty { get; set; }
     }
